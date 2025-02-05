12694c25a3d4a81998372e21bcca6f349805046f
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-02-05 21:49:48 +0600

    Create hadith.ts

---
+++ src/types/hadith.ts
@@ -0,0 +1,12 @@
+export interface HadithResponse {
+  data: {
+    book: string
+    bookName: string
+    chapterName: string
+    hadith_english: string
+    header: string
+    id: number
+    refno: string
+  }
+}
+
